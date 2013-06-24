6ca8d42ca0954a42033a6741bfdb8c50b63517ab
Phil Derksen <pderksen@gmail.com>
2013-06-23 22:05:48 -0700

    Now uninstalls general settings options

--- gumroad/uninstall.php
+++ gumroad/uninstall.php
@@ -14,7 +14,10 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-//Remove custom post meta fields
+// Remove option records from options table
+delete_option( 'gum_settings_general' );
+
+// Remove custom post meta fields
 $posts = get_posts( array( 'numberposts' => -1 ) );
 
 foreach( $posts as $post ) {
