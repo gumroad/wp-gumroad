78e2511da884f2b0d3ac67b2ffae863c8ac72cb5
Phil Derksen <pderksen@gmail.com>
2013-08-03 16:05:45 -0700

    Localization correction.

--- gumroad/class-gumroad.php
+++ gumroad/class-gumroad.php
@@ -152,7 +152,7 @@ class Gumroad {
 
 		$this->plugin_screen_hook_suffix = add_options_page(
 			$this->get_plugin_title() . __( ' Settings', 'gum' ),
-			__( 'Gumroad', $this->plugin_slug ),
+			__( 'Gumroad', 'gum' ),
 			'manage_options',
 			$this->plugin_slug,
 			array( $this, 'display_plugin_admin_page' )
